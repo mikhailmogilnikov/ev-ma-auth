a7646d08f6b058867fe1f85b41da3ad7624700d6
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2025-03-10 21:21:44 +0300

    refactor: add header in home page

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,11 +1,13 @@
 import { LogoutButton } from "@/features/logout";
 import { Container } from "@/shared/ui/primitives/container";
-
+import { Flex } from "@/shared/ui/primitives/flex";
 export default function HomePage() {
   return (
-    <Container yPadding className="flex items-center justify-between">
-      <h1 className="text-2xl font-bold">Home page</h1>
-      <LogoutButton />
+    <Container yPadding>
+      <Flex as="header" className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Home page</h1>
+        <LogoutButton />
+      </Flex>
     </Container>
   );
 }
